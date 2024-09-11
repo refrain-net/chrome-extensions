0ccece84efcf26e64927ad859d7b5c5af7c08534
Refrain <72150044+refrain-net@users.noreply.github.com>
2024-09-11 23:51:24 +0900

    Add files via upload

--- hide-zombie-account/script.js
+++ hide-zombie-account/script.js
@@ -1,9 +1,35 @@
 'use strict';
 
 /**
+ * @version 2
+ * @update 2024-09-11 ツイートしたアカウントは対象外にする
+ */
+//*
+requestAnimationFrame(function hide () {
+  const {href} = location;
+  if (!/https:\/\/x\.com\/.+\/status\/.+/.test(href)) return;
+  const [domain, userid, status, postid] = location.href.split('://')[1].split('/');
+  const elements = [...document.querySelectorAll('[data-testid="cellInnerDiv"]')];
+  const usernames = elements.map(element => [...element.querySelectorAll('a')].filter(anchor => (anchor.href.startsWith('https://twitter.com/') || anchor.href.startsWith('https://x.com/')) && anchor.textContent.startsWith('@'))[0]?.textContent).filter(username => username !== userid);
+  const count = usernames.reduce((acc, cur) => {
+    acc[cur] = (acc[cur] || 0) + 1;
+    return acc;
+  }, {});
+  let username;
+  elements.forEach((element, index) => {
+    username = usernames[index];
+    if (count[username] < 3) return;
+    element.style.display = 'none';
+  });
+  requestAnimationFrame(hide);
+});
+//*/
+
+/**
  * @version 1
  * @update 2024-09-09 リプライ数が3個以上のアカウントを非表示にする
  */
+/*
 requestAnimationFrame(function hide () {
   if (!/https:\/\/x\.com\/.+\/status\/.+/.test(location.href)) return;
   const elements = [...document.querySelectorAll('[data-testid="cellInnerDiv"]')];
@@ -20,3 +46,4 @@ requestAnimationFrame(function hide () {
   });
   requestAnimationFrame(hide);
 });
+//*/
